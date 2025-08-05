3a338f8d38eee2808dc1f5bff24e187538706536
Dheenadhayalan SS <selvarajkeerthana05@gmail.com>
2025-08-05 21:09:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (14.59%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,17 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isBalanced(TreeNode root) {
-        int[] result = dfs(root);
-        return result[0] == 1;        
-    }
+      public int maxDepth(TreeNode root) {
+        if(root==null)return 0;
 
-    private int[] dfs(TreeNode node) {
-        if (node == null) return new int[]{1, 0}; // {isBalanced (1 for true, 0 for false), height}
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if(left==-1||right==-1)return -1;
+        if(Math.abs(left-right)>1)return -1;
+        return 1+ Math.max(left,right);
+
+
+    }
+    public boolean isBalanced(TreeNode root) {
         
-        int[] left = dfs(node.left);
-        int[] right = dfs(node.right);
-        
-        boolean isBalanced = left[0] == 1 && right[0] == 1 && Math.abs(left[1] - right[1]) <= 1;
-        
-        return new int[]{isBalanced ? 1 : 0, 1 + Math.max(left[1], right[1])};
-    }    
+        if(maxDepth(root)==-1)return false;
+        return true;
+    }
 }
